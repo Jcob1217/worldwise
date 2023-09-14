a70149091daf24397fbd667e789ba6b635d0117f
Kuba Świętek <jackobs@interia.eu>
2023-09-14 13:41:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+The web application serves as a tool for tracking travels and registering exciting adventures. Its primary function is to empower users to document their journeys and visualize them on an interactive map. The application is crafted using React, with navigation facilitated by the react-router-dom library and state management handled through the context API.
+
+Notable Features:
+
+Adventure Registration: Users can effortlessly record their visited cities by completing a user-friendly form. This form typically collects information such as city name, visit date, and any personalized details they want to include.
+
+Map Visualization: The application integrates a dynamic map component, pinpointing each city the user has explored. Clicking on a marker provides in-depth insights into the city and the user's experiences there.
+
+React Router: For seamless navigation, the react-router-dom library is employed, enabling users to transition between different sections of the application, such as the homepage, the registration form, and the map view.
+
+Context API for State Management: The context API streamlines application state management, promoting efficient data sharing between components. This eliminates the need for cumbersome prop drilling and ensures smooth tracking and updating of the user's travel data.
---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import ProtectedRoute from "./pages/ProtectedRoute";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// import Homepage from "./pages/Homepage";
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import PageNotFound from "./pages/PageNotFound";
+// import AppLayout from "./pages/AppLayout";
+// import Login from "./pages/Login";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+
+// dist/assets/index-aebac544.css   30.17 kB │ gzip:   5.06 kB
+// dist/assets/index-e8777a22.js   524.39 kB │ gzip: 148.49 kB
+
+function App() {
+  return (
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }>
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
